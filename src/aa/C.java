0337c06c9e5138131b4f994aa23c1ef7b0857828
wangf <810833939@qq.com>
2015-09-26 20:20:37 +0800

    ddd

--- src/aa/C.java
+++ src/aa/C.java
@@ -10,7 +10,7 @@ public class C {
 		List<Integer> a = Arrays.asList(ak);
 		Integer al = a.stream().reduce(0, (acc,el)->acc+el);
 		System.out.println(al.intValue());
-		System.out.println("dd");
+		System.out.println("");
 //		a.stream().
 		/*Integer k = Collections.min(a);
 		Collections.
